3aab6b78f9335f03abe6a40c26b8b222c0d6d23f
Vivek raut <rautlata59@gmail.com>
2025-03-25 22:02:15 +0530

    Update Combination.c

--- functions and pointer/Combination.c
+++ functions and pointer/Combination.c
@@ -1,29 +1,41 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     int n;
     printf(" enter n : ");
-    scanf("%d",&n);
+    scanf("%d", &n);  // Input n
+
     int r;
     printf(" enter r :");
-    scanf("%d",&r);
-    int nfact = 1; //n!
-    int rfact = 1; //r!
-    int nrfact = 1; //n-r!
-    for ( int i =2; i<=n; i++){
-        nfact = nfact*i;
-    }
-    for ( int i=2; i<=n-r; i++){
-        nrfact = nrfact*i;
+    scanf("%d", &r);  // Input r
 
+    int nfact = 1;  // n!
+    int rfact = 1;  // r!
+    int nrfact = 1; // (n-r)!
+
+    // Calculate n!
+    for (int i = 2; i <= n; i++) {
+        nfact = nfact * i;
     }
-    int ncr = nfact/( rfact*nrfact);
-    printf("%d",ncr);
+
+    // Calculate r!
+    for (int i = 2; i <= r; i++) {
+        rfact = rfact * i;
+    }
+
+    // Calculate (n-r)!
+    for (int i = 2; i <= n - r; i++) {
+        nrfact = nrfact * i;
+    }
+
+    // Calculate nCr = n! / (r! * (n-r)!)
+    int ncr = nfact / (rfact * nrfact);
+
+    // Output the result
+    printf("%d", ncr);
+
     return 0;
-   
 }
-
-
-
 
 
 
